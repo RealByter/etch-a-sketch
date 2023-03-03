687b873957d8f0cd327351724fb5aff8212a3153
RealByter <justavishay@gmail.com>
2023-03-03 20:26:13 +0200

    feat: make the change grid button do its job

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const grid = document.getElementById('grid');
 const gridSize = 960;
 
 const generateGrid = squaresPerSide => {
@@ -7,8 +8,27 @@ const generateGrid = squaresPerSide => {
 		const newSquare = document.createElement('div');
 		newSquare.style.width = `${squareSize}px`;
 		newSquare.style.height = `${squareSize}px`;
+		newSquare.addEventListener('mouseover', e => {
+			e.target.style.backgroundColor = '#000000';
+		});
 		grid.appendChild(newSquare);
 	}
 };
+
+const changeGridButton = document.getElementById('change-grid');
+changeGridButton.addEventListener('click', () => {
+	let newSquaresPerSide = parseInt(
+		prompt('Enter the new number of square by size (maximum is 100):')
+	);
+
+	if (newSquaresPerSide > 100) {
+		newSquaresPerSide = 100;
+	} else if (newSquaresPerSide <= 1) {
+		newSquaresPerSide = 1;
+	}
+
+	grid.textContent = '';
+  generateGrid(newSquaresPerSide);
+});
 
 generateGrid(30);
